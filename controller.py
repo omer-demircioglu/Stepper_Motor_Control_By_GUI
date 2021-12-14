9dd262c9b0ced8a4b2808b81f3e6f7ff7c5062bb
omer-demircioglu <you@example.com>
2021-12-14 22:10:34 +0300

    d

---
+++ controller.py
@@ -0,0 +1,24 @@
+from PyQt5.QtWidgets import*
+from PyQt5.QtCore import pyqtSlot
+from gui import Ui_MainWindow
+
+class controller(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+    @pyqtSlot(int)
+    def on_horizontalSlider_valueChanged(self, current_index):
+
+        self.ui.label.setText("değer = " + str(current_index))
+
+
+uygulama = QApplication([])
+pencere = dersler_7()
+pencere.show()
+uygulama.exec_()
+
+
